7f4e2dabb550d586ef691ef289e9ba1dd957e582
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2023-10-16 19:17:37 -0300

    create methods valorPretendido and selecaoCandidatos.

--- src/candidatura/ProcessoSetivo.java
+++ src/candidatura/ProcessoSetivo.java
@@ -1,14 +1,35 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSetivo {
     public static void main(String[] args) {
         System.out.println("Processo Seletivo!");
-
-        analisarCandidatos(1900.0);
-        analisarCandidatos(2200.0);
-        analisarCandidatos(2000.0);
+        selecaoCandidatos();
     }
 
+    static void selecaoCandidatos(){
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salário " + salarioPretendido );
+            if (salarioBase >= salarioPretendido ){
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga.");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+     }
+
+     static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2000);
+     }
     static void analisarCandidatos(double salarioPretendido){
         double salarioBase = 2000.0;
         if (salarioBase > salarioPretendido){
